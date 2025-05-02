db924ddc8503c9c233663d69a211eac1fa7c092d
tushar <tusharchemate07@gmail.com>
2025-05-02 06:27:57 +0530

    feat: Initialize project

---
+++ src/main.tsx
@@ -0,0 +1,38 @@
+import {StrictMode} from 'react'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import Landing from './pages/landing/Landing';
+
+export const routes = [
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <Landing />
+			}
+		]
+	}
+];
+
+const router = createBrowserRouter(routes);
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: false,
+			cacheTime: 1000 * 60 * 15
+		}
+	}
+});
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</StrictMode>
+);
