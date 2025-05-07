8799addee2304673a61856f45e0c98c3ac2732b2
tushar <tusharchemate07@gmail.com>
2025-05-08 02:16:18 +0530

    feat: Add index file for page exports and streamline router imports

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as Landing } from "./Landing/Landing";
+export { default as Login } from "./Login/Login";
+export { default as Dashboard } from "./Dashboard/Dashboard";
+export { default as ResourceList } from "./ResourceList/ResourceList";
+export { default as ResourceDetail } from "./ResourceDetails/ResourceDetail";
--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,6 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
-import Landing from "./pages/Landing/Landing";
-import Login from "./pages/Login/Login";
+import { Landing, Login, Dashboard, ResourceList, ResourceDetail } from "./pages";
 import PrivateRoute from "./components/PrivateRoute";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import ResourceList from "./pages/ResourceList/ResourceList";  
-import ResourceDetail from "./pages/ResourceDetails/ResourceDetail"; 
 
 export const route = createBrowserRouter([
     {
