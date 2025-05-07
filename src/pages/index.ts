36aab3c0ce25e00286a0e3a6eac48ef40da988ec
tushar <tusharchemate07@gmail.com>
2025-05-08 03:20:08 +0530

    fix: correct import path casing for Landing component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as Landing } from "./landing/Landing";
+export { default as Landing } from "./landing/landing";
 export { default as Login } from "./login/login";
 export { default as Dashboard } from "./dashboard/dashboard";
 export { default as ResourceList } from "./resourceList/resourceList";
