0a18005335d786d7a56873c95bffb770c686038b
tushar <tusharchemate07@gmail.com>
2025-05-08 02:58:51 +0530

    refactor(pages): change Login export to direct import

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { default as Landing } from "./landing/Landing";
-export { default as Login } from "./login/Login";
+import Login from "./login/Login";
 export { default as Dashboard } from "./dashboard/Dashboard";
 export { default as ResourceList } from "./resourceList/ResourceList";
 export { default as ResourceDetail } from "./resourceDetails/ResourceDetail";
